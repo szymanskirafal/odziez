954f5dc7e9740bffbb3d5bcbdd2557218c865df2
Rafal Szymanski <r.szymansky@gmail.com>
2019-05-27 13:57:50 +0200

    Add admin to models: Etat, MiejscePracy, Nadzorca, Pracownik, stanowisko.

--- odziez/pracownicy/admin.py
+++ odziez/pracownicy/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Etat, MiejscePracy, Nadzorca, Pracownik, Stanowisko
+
+
+@admin.register(Etat)
+class EtatAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MiejscePracy)
+class MiejscePracyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Nadzorca)
+class NadzorcaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Pracownik)
+class PracownikAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Stanowisko)
+class StanowiskoAdmin(admin.ModelAdmin):
+    pass
