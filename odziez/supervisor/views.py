d52c1bac9db33b8e7be466e17aa2c58b55bd6f8b
Rafal Szymanski <r.szymansky@gmail.com>
2020-05-07 09:09:42 +0200

    fix

--- odziez/supervisor/views.py
+++ odziez/supervisor/views.py
@@ -114,7 +114,6 @@ class SupervisorOrderUpdateView(
             body = 'W aplikacji jest nowe zamówienie',
             from_email = from_email,
             to = [to, ],
-            cc = [to, ],
         )
         email.send()
         return super().form_valid(form)
